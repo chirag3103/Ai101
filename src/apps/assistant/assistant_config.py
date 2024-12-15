85d47718eeb726a37fef97c788b56508e3603122
chirag <chiragagarwal3103@gmail.com>
2024-12-14 20:44:59 -0800

    Updates prompt file path comment

--- src/apps/assistant/assistant_config.py
+++ src/apps/assistant/assistant_config.py
@@ -1,6 +1,6 @@
 def get_placeholder_assistant_config(model_name):
     return {
-        "name": "placeholder",  # directory name under /content/prompts (regardless of version)
+        "name": "placeholder",  # directory name under /content/prompts/v1 (regardless of version)
         "model_name": model_name,
         "system_content_version": 1,
         "user_content_version": 1,
